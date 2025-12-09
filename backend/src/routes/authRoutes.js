8185ca22f10bcc0ee328fa18ff461cc06fcf0244
afrismartfarming <afrismartfarming@gmail.com>
2025-12-09 21:20:01 +0000

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 // backend/src/routes/authRoutes.js
 import express from "express";
+import mongoose from "mongoose";
 
 import {
   registerUser,
@@ -14,6 +15,13 @@ import {
 import { protect, authorize } from "../middleware/auth.js";
 
 const router = express.Router();
+
+router.get("/debug", (req,res)=>res.json({
+   connectedTo: mongoose.connection.name,
+   uri: process.env.MONGO_URI ? "LOADED" : "NOT_LOADED",
+   jwt: process.env.JWT_SECRET ? "SET" : "MISSING"
+}));
+
 
 /* ----------------------------------------------------------
    PUBLIC ROUTES
